5c0af52be41f165789ecb55d3f9c1ec6eae0e300
klassikcat <shinjeongtae@gmail.com>
2025-05-24 08:45:07 +0000

    [FIX] move awscliv2 installtaion from dockerfile to bash script

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+arch=$(uname -m)
+if [ "$arch" = "x86_64" ]; then
+    architecture="amd64"
+elif [ "$arch" = "aarch64" ] || [ "$arch" = "arm64" ]; then
+    architecture="arm64"
+else
+    echo "Your CPU architecture is not supported. exit."
+    exit 1
+fi
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-$architecture.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install
+rm awscliv2.zip
+rm -rf aws
+
 git config --global core.editor 'vim'
 echo 'export EDITOR=vim' >> ~/.bashrc
 echo 'export VISUAL=vim' >> ~/.bashrc
